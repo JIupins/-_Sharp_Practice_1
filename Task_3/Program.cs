dd541c0e5ef16a6631e7b5b90fde1396418ff73b
AlekseyBaboshin <BaboshinAP@gmail.com>
2022-11-22 23:38:19 +0300

    Created a program into Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (Convert.ToBoolean(A%2)==false)
+{
+    Console.WriteLine($"Число {A} четное.");
+}
+else
+{
+    Console.WriteLine($"Число {A} не четное.");
+}
