aadff1bb505361a357d79702f40f75d3af03723d
AlekseyBaboshin <BaboshinAP@gmail.com>
2022-11-22 23:07:32 +0300

    Created a program into Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите первое число: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int C = int.Parse(Console.ReadLine());
+int T = Math.Max(Math.Max(A,B),C);
+Console.WriteLine($"Максимальным из чисел {A}, {B} и {C} является: {T}");
