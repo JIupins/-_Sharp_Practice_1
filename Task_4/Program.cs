2cdd90341cbb9ea106d37f9f5f0fa343e9675d3a
AlekseyBaboshin <BaboshinAP@gmail.com>
2022-11-23 00:30:07 +0300

    Created a program into Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = 0;
+for (int count = 1; count < A; count++)
+{
+    B = count;
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count}, ");
+    }
+}
+if (B < 2)
+{
+    Console.Write($"Четных чисел перед числом {A} нет!");
+}
