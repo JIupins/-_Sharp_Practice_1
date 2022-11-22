aa4b87fb8e81e022d17a0357a07854127ba4f379
AlekseyBaboshin <BaboshinAP@gmail.com>
2022-11-22 22:49:21 +0300

    Created program into Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = int.Parse(Console.ReadLine());
+int T = Math.Max(A,B);
+Console.WriteLine($"Максимальным из чисел {A} и {B} является: {T}");
