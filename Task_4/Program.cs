43c729e769ace9b2472838cdb68f639a925a38b6
Aleksey <105822544+JIupins@users.noreply.github.com>
2022-11-24 10:42:41 +0300

    Chenged names of variables

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите число: ");
-int A = Convert.ToInt32(Console.ReadLine());
-int B = 0;
-for (int count = 1; count < A; count++)
+int one = Convert.ToInt32(Console.ReadLine());
+int two = 0;
+for (int count = 1; count < one; count++)
 {
     B = count;
     if (count % 2 == 0)
@@ -9,7 +9,7 @@ for (int count = 1; count < A; count++)
         Console.Write($"{count}, ");
     }
 }
-if (B < 2)
+if (two < 2)
 {
-    Console.Write($"Четных чисел перед числом {A} нет!");
+    Console.Write($"Четных чисел перед числом {one} нет!");
 }
